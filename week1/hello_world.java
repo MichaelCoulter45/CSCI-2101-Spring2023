5bd3b10c9946ec39e7d8041dc614b377416c4019
Michael Coulter <coulte12@go.stockton.edu>
2023-01-26 18:53:47 -0500

    .

--- week1/hello_world.java
+++ week1/hello_world.java
@@ -2,6 +2,6 @@ package week1;
 
 public class hello_world {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("hello World!");
     }
 }
