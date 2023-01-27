94be937b7f63e7eaa22059183cebdfcfd682609c
Michael Coulter <coulte12@go.stockton.edu>
2023-01-26 19:17:22 -0500

    My first program

--- week1/hello_world.java
+++ week1/hello_world.java
@@ -1,8 +1,26 @@
 package week1;
 
 public class hello_world {
+int i=0;
     public static void main(String[] args) {
+        int i=0;
         System.out.println("Hello World!");
         System.out.println("This is our first Java program!");
+        System.out.println("Butts");
+        System.out.println("Butts ");
+        System.out.println("Butts ");
+while (i<3){
+    System.out.println("Butts " + (i+1));
+    i++;
+}
     }
 }
+
+
+/*
+ * End of file
+*/
+
+// how do i print variables in string? i tried %i but it didnt work
+
+
